dc334ace446332bf0861b31146581db01d41d92f
Greg Wright <gregwrightbjj@gmail.com>
2015-02-20 12:49:54 -0500

    looking good

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ var getUserAccount = function(){
 		},
 		method: "GET",
 		success: function(userData){
-			console.log(userData);
-var userObject = {
-avatar: userData.avatar_url,
-name: userData.name,
-userName: userData.login,
-company: userData.company,
-location: userData.location,
-email: userData.email,
-joinedDate: userData.created_at,
-blog: userData.blog,
-Followers: userData.followers,
-Following: userData.following
+			console.log(userData)
+	var userObject = {
+	avatar: userData.avatar_url,
+	name: userData.name,
+	userName: userData.login,
+	company: userData.company,
+	location: userData.location,
+	email: userData.email,
+	joinedDate: userData.created_at,
+	blog: userData.blog,
+	Followers: userData.followers,
+	Following: userData.following
 }
 // Creates an html string with the Handlebars template and our new object
    var htmlString = templates.userProf(userObject)
@@ -45,20 +45,25 @@ var getUserRepo = function(){
 		method: "GET",
 		success: function(repoData){
 			console.log(repoData);
-var repoObject = {
-name: repoData.name,
-language: repoData.language,
-stargazers: repoData.stargazers_count,
-forkCount: repoData.fork_count,
-fork_url: repoData.fork_url,
-description: repoData.description,
-updated: repoData.updated_at
-}
-// Creates an html string with the Handlebars template and our new object
-var htmlString2= templates.repos(repoObject)
-// Append that HTML string to the container on the page
- $("#repo-container").append(htmlString2);
 
+			_.each(repoData, function(repository){
+
+		var repoObject = {
+		name: repository.name,
+		language: repository.language,
+		stargazers: repository.stargazers_count,
+		forkCount: repository.fork_count,
+		fork_url: repository.fork_url,
+		description: repository.description,
+		updated: repository.updated_at
+		}
+
+		// Creates an html string with the Handlebars template and our new object
+		var htmlString2= templates.repos(repoObject)
+		// Append that HTML string to the container on the page
+ 		$("#repo-container").append(htmlString2);
+			})
+	
 		}
 	})
 }
@@ -87,5 +92,5 @@ $(document).on("ready", function(){
 
 	getTemplates()
 	getUserAccount()
-	getUserRepo();
+	getUserRepo()
 })
